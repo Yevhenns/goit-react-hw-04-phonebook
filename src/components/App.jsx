c7182237bff1e833b45a044d7a4a54f92e4db46a
Yevhenns <103356318+Yevhenns@users.noreply.github.com>
2022-11-27 10:03:24 +0200

    Update App.jsx

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -7,13 +7,16 @@ import { nanoid } from 'nanoid';
 import css from './App.module.css';
 
 export const App = () => {
-  const [contacts, setContacts] = useState([
-    { id: 'id-1', name: 'Rosie Simpson', number: '459-12-56' },
-    { id: 'id-2', name: 'Hermione Kline', number: '443-89-12' },
-    { id: 'id-3', name: 'Eden Clements', number: '645-17-79' },
-    { id: 'id-4', name: 'Annie Copeland', number: '227-91-26' },
-  ]);
+  const [contacts, setContacts] = useState(() => {
+    return JSON.parse(window.localStorage.getItem('contacts')) ?? [];
+  });
   const [filterData, setFilterData] = useState('');
+
+  useEffect(() => {
+    if (contacts) {
+      window.localStorage.setItem('contacts', JSON.stringify(contacts));
+    }
+  }, [contacts]);
 
   const deleteContact = id => {
     setContacts(contacts.filter(contact => contact.id !== id));
